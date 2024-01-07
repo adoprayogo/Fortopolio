0f04638a43e4fcc15562263586b1426a72d012af
unknown <adoprayogo22@gmail.com>
2024-01-07 11:15:54 +0700

    portofolio

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: [' Web Developer', ' Geologist', ' Any Other Role'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+        
+    };
+
+    var typed = new Typed('.text', options);
+});
